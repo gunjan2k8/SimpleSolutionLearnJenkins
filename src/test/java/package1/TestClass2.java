90838cf39ceddec3cecd759b6d1d5f8fb2b3650d
gunjan <gunjan2k8@gmail.com>
2022-07-23 20:09:24 +0530

    changes

--- src/test/java/package1/TestClass2.java
+++ src/test/java/package1/TestClass2.java
@@ -1,5 +1,13 @@
 package package1;
 
+import org.testng.annotations.Test;
+
 public class TestClass2 {
+	
+	@Test
+	public void OpenBrowser() {
+		  System.out.println("This will open browser");
+	  }
+
 
 }
